5d64d0ecd72bb9f6574d38295e0d70d2c8dba6a5
Yerniyaz Dossanov <dosanov.erniyaz@gmail.com>
2021-05-21 22:17:58 +0600

    Fix for iOS devices

--- script.js
+++ script.js
@@ -223,3 +223,11 @@ document.addEventListener('keyup', (e) => {
         if (!isFirstPage()) setPrevSlide();
     }
 });
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
